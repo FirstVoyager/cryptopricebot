a9b144488e076e4e10120fb5e43d2d066a502bab
FirstVoyager <miladsohrabif@gmail.com>
2022-08-16 14:02:57 +0200

    Add more coins

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import json
 import requests
 import yaml
+import os
 from time import sleep
 from sender.sender import Sender
 
 
 def main():
     # Read config
-    yaml_file = open("config.yml", "r", encoding="utf-8")
+    config_file = "config.yml"
+    if os.path.isfile('config_dev.yml'):
+        config_file = 'config_dev.yml'
+    yaml_file = open(config_file, "r", encoding="utf-8")
     config = yaml.load(yaml_file, Loader=yaml.FullLoader)
     print(config["binance_ticker_api"])
     sender = Sender(telegramBotToken=config['telegramBotToken'], telegramChannelId=config['telegramChannelId'])
@@ -16,11 +20,23 @@ def main():
     while True:
         # load btc price form binance
         data = json.loads(requests.get(config['binance_ticker_api']).text)
-        msg = "{}$".format(float(data['price']))
-        if "." in msg:
-            msg = msg.strip('0')
+        newList = []
+        for i in data:
+            if i['symbol'] in config['cryptoList']:
+                newList.append(i)
+
+        msg = ''
+        for i in newList:
+            price = str(i['price'])
+            if '.' in price:
+                price = str(price).rstrip('0')
+            msg += i['symbol'] + ": " + price + '$' + '\n'
+
+        # msg = "{}$".format(float(data['price']))
+#         if "." in msg:
+#             msg = msg.strip('0')
         sender.send(msg)
-        sleep(5)
+        sleep(config['delayTime'])
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("0240.2000".strip('0'))
+import os
+import yaml
+
+config_file = "config.yml"
+if os.path.isfile('config_dev.yml'):
+    config_file = 'config_dev.yml'
+yaml_file = open(config_file, "r", encoding="utf-8")
+config = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+print(config['cryptoList'])
+for i in config['cryptoList']:
+    print(i)
